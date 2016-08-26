a1413207eb88c531fcf31be27e18da7df7022bcf
nas <nas@9b05d33b-1858-46e7-9e32-5675b235c192>
2016-08-26 09:25:51 +0000

    addMergeContent bufix

---
+++ tags/0.0.6/ResponseInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace TrunkSoftware\Component\Http;
+
+interface ResponseInterface {
+
+	public function setContent( array $data );
+	public function addContent( $name, $value );
+	public function getContent( $name );
+	public function getCode();
+	public function setCode( $code );
+}
