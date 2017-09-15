0a09ac21e5c6fbaa4c464eb17ac069b679d42861
chris <chris@9b05d33b-1858-46e7-9e32-5675b235c192>
2017-09-15 16:26:36 +0000

    Tagged 0.0.9

---
+++ tags/0.0.9/Request.php
@@ -0,0 +1,75 @@
+<?php
+namespace TrunkSoftware\Component\Http;
+
+class Request
+{
+	protected $get = array();
+	protected $post = array();
+	protected $raw = array();
+
+	public function __construct( array $get, array $post, array $raw = null ) {
+		$this->get = $get;
+		$this->post = $post;
+		$this->raw = $raw;
+	}
+
+	public function raw( $name, $default = null ) {
+		if( isset( $this->raw[ $name ] ) ) {
+			return $this->raw[ $name ];
+		}
+
+		return $default;
+	}
+
+	public function post( $name, $default = null ) {
+		if( isset( $this->post[ $name ] ) ) {
+			return $this->post[ $name ];
+		}
+
+		return $default;
+	}
+
+	public function get( $name, $default = null ) {
+		if( isset( $this->get[ $name ] ) ) {
+			return $this->get[ $name ];
+		}
+
+		return $default;
+	}
+
+	public function request( $name, $default = null ) {
+		if( isset( $this->get[ $name ] ) ) {
+			return $this->get[ $name ];
+		}
+		if( isset( $this->post[ $name ] ) ) {
+			return $this->post[ $name ];
+		}
+		if ( isset( $this->raw[ $name ] ) ) {
+			return $this->raw[ $name ] ;
+		}
+
+		return $default;
+	}
+
+	public function toArray( $global = null ) {
+		switch($global){
+			case "post":
+			case "POST":
+				$results = $this->post;
+				break;
+			case "get":
+			case "GET":
+				$results = $this->get;
+				break;
+			case "raw":
+			case "RAW":
+				$results = $this->raw;
+				break;
+			default:
+				$results = array_merge( array(), $this->get, $this->post, $this->raw );
+				break;
+		}
+
+		return $results;
+	}
+}
