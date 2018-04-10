b60addf9a244856433e73d1a5a068881e58e8638
chris <chris@9b05d33b-1858-46e7-9e32-5675b235c192>
2018-04-10 14:01:58 +0000

    Tagged 0.0.11

---
+++ tags/0.0.11/Response.php
@@ -0,0 +1,131 @@
+<?php
+namespace TrunkSoftware\Component\Http;
+use TrunkSoftware\Component\Errors\ErrorInterface;
+use TrunkSoftware\Component\Errors\ErrorBag;
+
+/**
+ * Class Response
+ * @package TrunkSoftware\Component\Http
+ */
+class Response implements ResponseInterface {
+
+	/**
+	 * @var ErrorBag
+	 */
+	private $errors;
+
+	/**
+	 * @var array
+	 */
+	private $data;
+
+	/**
+	 * response code e.g. HTTP status code
+	 * @var int|null
+	 */
+	private $code = 200;
+
+	public function __construct( $data = array(), $code = null ) {
+
+		$this->errors = new ErrorBag();
+
+		$this->data = $data;
+		$this->code = $code;
+
+	}
+
+	public function setContent( array $data ) {
+		$this->data = $data;
+		return $this;
+	}
+
+	public function addMergeContent( array $data ) {
+		if( !$this->errors->getCount() )
+			$this->data = array_merge( $this->data, $data );
+		return $this;
+	}
+
+	public function addContent( $name, $value = null ) {
+		if(is_array( $name )) {
+			foreach( $name as $name => $value ) {
+				$this->data[ $name ] = $value;
+			}
+		} else {
+			$this->data[ $name ] = $value;
+		}
+
+		return $this;
+	}
+
+	public function removeContent( $name ) {
+		if( isset( $this->data[ $name ]) ) {
+			unset( $this->data[ $name ] );
+		}
+		return $this;
+	}
+
+	public function clearContent() {
+		$this->data = array();
+		return $this;
+	}
+
+	public function getContent( $name = null ) {
+		return !$name ? $this->data :
+				(isset($this->data[ $name ]) ? $this->data[ $name ] : NULL);
+	}
+
+	public function getCode() {
+		return $this->code;
+	}
+
+	public function setCode( $code ) {
+		$this->code = $code;
+		return $this;
+	}
+
+	public function setError( ErrorInterface $error ) {
+		$this->setCode( $error->getErrorCode() );
+		$this->addError( $error );
+		return $this;
+	}
+
+	public function addError( ErrorInterface $error ) {
+		$this->errors->addError( $error );
+		return $this;
+	}
+
+	public function getErrors() {
+		return $this->errors;
+	}
+
+	public function hasErrors() {
+		return $this->errors->getCount();
+	}
+
+	/**
+	 * Output the response
+	 */
+	public function render_output() {
+
+		// If the code is not 200
+		if ( $this->code !== 200 ) {
+			// Output the correct response code
+			http_response_code( $this->code );
+		}
+
+		// If there are errors
+		if ( $this->errors ) {
+			// Iterate over the errors
+			foreach( $this->errors as $error ) {
+				// Output the error message
+				echo $error->getMessage() . "\n";
+			}
+		}
+
+		$this->_render_output();
+	}
+
+	protected function _render_output() {
+
+	}
+}
